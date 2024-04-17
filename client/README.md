b216f0c97baf668eebe08938b8dd64d43c8562ac
Patricio Parada <paradagpatricio@gmail.com>
2024-04-17 17:21:13 -0400

    Add frontend skeleton
    Right now is only the skeleton, and a few other configurations.
    
    TODO:
    - implement the form component to retrieve user data
    - implement a way to display all posts created by the users.
      - maybe a grid?
      - maybe a list?
      - maybe a table?

---
+++ client/README.md
@@ -0,0 +1,5 @@
+# Like Me!
+
+## What is going on here?
+
+~~Breakfast!~~ This is the frontend part for this challenge. I'm not gonna explain a lot here since this isn't the main learning goal for this module. I'm using `antd` as framework and `react-router-dom` to handle routing (unnecessary, since it's only on page so far).
---
+++ client/src/components/Home.tsx
@@ -0,0 +1,69 @@
+import { Button, Divider, Layout } from 'antd';
+import { CameraTwoTone, LikeFilled, MenuOutlined } from '@ant-design/icons';
+
+const { Header, Content, Footer } = Layout;
+
+export const Home = () => {
+
+  return (
+    <>
+      <Header
+        style={{
+          position: 'sticky',
+          top: 0,
+          zIndex: 1,
+          color: 'rgb(244, 219, 214)',
+          backgroundColor: 'rgb(73, 77, 100)',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          textAlign: 'center',
+          fontSize: '1.5rem',
+        }}
+      >
+        <div>
+          <h2><CameraTwoTone twoToneColor={"#8aadf4"} /></h2>
+        </div>
+        <div>
+          <h2>Like Me! <LikeFilled rotate={-30} style={{ color: '#7dc4e4' }} /></h2>
+        </div>
+        <div>
+          <Button type='text'>
+            <h2><MenuOutlined style={{ color: '#7dc4e4' }} /></h2>
+          </Button>
+        </div>
+      </Header>
+      <Content
+        style={{
+          padding: '0 1.5rem',
+          margin: '1rem 0',
+        }}
+      >
+        <div
+          style={{
+            padding: 24,
+            backgroundColor: '#fff',
+            borderRadius: '10px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Divider><h2>New Post</h2></Divider>
+          {/* TODO: form component to retrieve data when creating new posts */}
+          <Divider><h2>Posts</h2></Divider>
+          {/* TODO: some sort of grid/table to show all posts created */}
+        </div>
+      </Content>
+      <Footer
+        style={{
+          color: 'rgb(244, 219, 214)',
+          backgroundColor: 'rgb(73, 77, 100)',
+          textAlign: 'center',
+        }}
+      >
+        Like Me! ©{new Date().getFullYear()} Created by Pela Fustán
+      </Footer>
+    </>
+  )
+};
